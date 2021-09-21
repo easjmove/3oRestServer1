4275ec189ea28419d9120ef36b6076c74518f90d
move <move@zealand.dk>
2021-09-21 11:21:46 +0200

    Added Fruit controller + manager And HTTP status codes, and some filtering

---
+++ RestServer1/Controllers/FruitController.cs
@@ -0,0 +1,68 @@
+﻿using JsonFruit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestServer1.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestServer1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitController : ControllerBase
+    {
+        public FruitManager _manager = new FruitManager();
+
+        // GET: api/<FruitController>
+        [HttpGet]
+        public IEnumerable<Fruit> Get()
+        {
+            return _manager.GetList(null);
+        }
+
+        // GET api/<FruitController>/5
+        [HttpGet("{substring}")]
+        public IEnumerable<Fruit> Get(string substring)
+        {
+            return _manager.GetList(substring);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet("Name")]
+        public ActionResult<List<Fruit>> GetFromUrl([FromQuery] string substring)
+        {
+            List<Fruit> result = _manager.GetList(substring);
+            if (result.Count > 0)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        // POST api/<FruitController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FruitController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FruitController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RestServer1/Manager/FruitManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JsonFruit;
+
+namespace RestServer1.Manager
+{
+    public class FruitManager
+    {
+        public static List<Fruit> fruits;
+
+        public FruitManager()
+        {
+            fruits = new List<Fruit>();
+            fruits.Add(new Fruit("Banana"));
+            fruits.Add(new Fruit("Apple"));
+            fruits.Add(new Fruit("Rotten Banana"));
+            fruits.Add(new Fruit("Good Apple"));
+        }
+
+        public List<Fruit> GetList(string substring)
+        {
+            List<Fruit> result = new List<Fruit>(fruits);
+            if (substring != null)
+            {
+                result = result.FindAll(fruit => fruit.typeOfFruit.Contains(substring,StringComparison.OrdinalIgnoreCase));
+            }
+            return result;
+        }
+    }
+}
